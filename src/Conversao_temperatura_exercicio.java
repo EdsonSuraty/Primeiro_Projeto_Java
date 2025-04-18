f672b75435feac818d22ebffede6bb1c3c551edd
EdsonSuraty <edinhosuraty@gmail.com>
2025-04-18 17:37:50 -0300

    refactor: refatoring code

--- src/Conversao_temperatura_exercicio.java
+++ src/Conversao_temperatura_exercicio.java
@@ -1,26 +1,16 @@
 public class Conversao_temperatura_exercicio {
     public static void main(String[] args) {
-
         double temperaturaCelsius = 32;
-        String mensagem = """
-                          A temperatura %.2f°, em Celsius é igual a %.2f em Fahrenheit""".formatted(temperaturaCelsius, converterTemperatura(temperaturaCelsius));
-
+        String mensagem = String.format("A temperatura %.2f°, em Celsius é igual a %.2f em Fahrenheit",temperaturaCelsius, converterTemperatura(temperaturaCelsius));
         System.out.println(mensagem);
 
         int fahrenheitInteiro = (int) converterTemperatura (temperaturaCelsius);
-
-        String mensagemFahrenheitInteiro = """
-                          O valor inteiro da temperatura em Fahrenheit é: %d""".formatted(fahrenheitInteiro);
+        String mensagemFahrenheitInteiro = String.format("O valor inteiro da temperatura em Fahrenheit é: %d", fahrenheitInteiro);
 
         System.out.println(mensagemFahrenheitInteiro);
-
     }
-
-
+    
     public static double converterTemperatura(double temperaturaCelsius){
-
         return (temperaturaCelsius * 1.8) +32;
-
     }
-
 }
